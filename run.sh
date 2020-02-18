5face7cd9c777ab36ebc71d1cdfff8dfe33f7df6
Tong Li <litong01@us.ibm.com>
2020-02-18 17:39:31 +0000

    Fixed the path issue
    Signed-off-by: Tong Li <litong01@us.ibm.com>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 docker run -dit --network minifab --name avalon --rm \
   -v $(pwd)/vars/keyfiles:/keyfiles \
-  -v $(pwd)/apps/src:/pysrc \
+  -v $(pwd)/apps:/pysrc \
   -v $(pwd)/vars/mychannel_network.json:/pysrc/network.json \
   hfrd/tongpy:3.6.9
 
