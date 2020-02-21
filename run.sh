dca123d501cc86f9dbf70fb155854578ce62794e
litong01 <litong01@users.noreply.github.com>
2020-02-21 13:54:27 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ docker run -dit --network minifab --name avalon --rm \
   -v $(pwd)/vars/keyfiles:/keyfiles \
   -v $(pwd)/apps:/pysrc \
   -v $(pwd)/vars/mychannel_network.json:/pysrc/network.json \
-  hfrd/tongpy:3.6.9
+  hfrd/tongpy:latest
 
